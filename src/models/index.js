cdee202f6ce5c07bda1c4c0866dd3a8e854629ca
phamquoctuan <72914569+phamquoctuan1@users.noreply.github.com>
2021-12-19 23:23:08 +0700

    config db

--- src/models/index.js
+++ src/models/index.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.PRODUCTION_PASSWORD,
   {
     host: process.env.PRODUCTION_HOST,
-    dialect: process.env.dialect,
+    dialect: process.env.PRODUCTION_DIALECT,
     operatorsAliases: 0,
     logging: true,
 
