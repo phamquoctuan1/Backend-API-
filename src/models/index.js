53c74ab3c7ec627bdbcb75a6de04bdd64bddc836
phamquoctuan <72914569+phamquoctuan1@users.noreply.github.com>
2021-12-19 23:56:48 +0700

    fix

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,7 @@
 const dbConfig = require('../config/db.config.js');
 require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
+
 const sequelize = new Sequelize(
   process.env.PRODUCTION_DATABASE,
   process.env.PRODUCTION_USER,
